3388e6422848fb995c104f9ef601ca66ff424e99
Timbosibguti <farrukh.kudratov.95@mail.ru>
2016-04-21 19:52:47 +0600

    nachalo

---
+++ src/app.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <math.h>
+
+int uravn(double a, double b, double c, double *x1, double *x2)
+{
+	//
+}
---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "app.h"
+
+int main()
+{
+	double a, b, c, x1, x2;
+	printf("vvedite a b c: ");
+	scanf("%d %d %d", &a, &b, &c);
+	koren = uravn(a, b, c, &x0, &x1);
+}
