fcc21cc5c2e9825722ea7cab2c08a47479840a91
Timbosibguti <farrukh.kudratov.95@mail.ru>
2016-04-22 22:24:16 +0600

    gotovo

--- src/main.c
+++ src/main.c
@@ -3,9 +3,23 @@
 
 int main()
 {
-	double a, b, c, x1, x2;
+	double a, b, c, d, x1, x2;
 	int koren;
 	printf("vvedite a b c: ");
 	scanf("%d %d %d", &a, &b, &c);
-	koren = uravn(a, b, c, &x0, &x1);
+	koren = uravn(a, b, c, &d, &x0, &x1);
+	if (koren == -1)
+	{
+		printf("a dolzhen byt ne raven nulju\n");
+		return 0;
+	}
+	
+	printf("D: %2lf.\n", d);
+	
+	if (koren == 0)
+		printf("kornej net\n");
+	else if (koren == 1)
+		printf("x = %.2lf.\n", x1);
+	else
+		printf("x1: %.2lf. x2: %.2lf.\n", x1, x2);
 }
