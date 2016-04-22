570906ac598aa39895ef6c0ed3ca8e9f0fea1b1f
Timbosibguti <farrukh.kudratov.95@mail.ru>
2016-04-22 22:14:40 +0600

    nachalo

--- src/app.c
+++ src/app.c
@@ -3,5 +3,15 @@
 
 int uravn(double a, double b, double c, double *x1, double *x2)
 {
-	//
+	int d;
+	if (a == 0)
+		return -1;
+	d = (b * b) - (4 * a * c);
+	if (d < 0)
+		return 0;
+	if (d == 0)
+	{
+		x1 = -b / (2 * a);
+		return 1;
+	}
 }
--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 int main()
 {
 	double a, b, c, x1, x2;
+	int koren;
 	printf("vvedite a b c: ");
 	scanf("%d %d %d", &a, &b, &c);
 	koren = uravn(a, b, c, &x0, &x1);
